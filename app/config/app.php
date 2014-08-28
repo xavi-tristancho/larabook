ad7f6958907cf87d07fbab91f93fb204448f7ab5
xavitb3 <xavitb3@gmail.com>
2014-08-28 12:49:20 +0200

    Arreglat el seguiment d'arxius que no haurien de seguir-se

--- app/config/app.php
+++ app/config/app.php
@@ -124,7 +124,7 @@ return array(
         'Way\Generators\GeneratorsServiceProvider',
         'Laracasts\Commander\CommanderServiceProvider',
         'Laracasts\Validation\ValidationServiceProvider',
-
+        'Laracasts\Flash\FlashServiceProvider'
 	),
 
 	/*
@@ -191,7 +191,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-
+        'Flash'             => 'Laracasts\Flash\Flash'
 	),
 
 );
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -13,9 +13,12 @@
     @include('layouts.partials.nav')
 
     <div class="container">
+        @include('flash::message')
         @yield('content')
     </div>
 
+    <script src="http://code.jquery.com/jquery.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script>$('#flash-overlay-modal').modal();</script>
 </body>
 </html>
